fcd8e7960111b19ba93f2dc9f15b33a73457d44c
Pritam252 <87931517+Pritam252@users.noreply.github.com>
2022-07-22 22:15:15 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,8 +1,7 @@
 @echo off
 echo Downloading Files:
 cd python.exe
-python ../mods/setup/mod_updater.py
-cd ..
+python mods/mod_updater.py
 
 echo Starting GitCraft152
 java -Xmx8g -Xms8g -jar server.jar nogui
